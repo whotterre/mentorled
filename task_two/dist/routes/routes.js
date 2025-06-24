d66501934a119c45f0e57a9a0868ad0b91ca7969
Iwegbu Jeddy <76275625+whotterre@users.noreply.github.com>
2025-06-24 16:31:26 +0100

    Delete task_two/dist directory

--- task_two/dist/routes/routes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express = require("express");
-const router = express.Router();
-const authController_1 = require("../controllers/authController");
-router.post("/register", authController_1.signupUser);
-router.post("/login", authController_1.loginUser);
-router.get("/profile", authController_1.getProfile);
-exports.default = router;
