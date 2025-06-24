13313dab4bb65a3ebff90937bbdc49f85e903c6b
whotterre <iJeddy5.0@gmail.com>
2025-06-24 16:29:32 +0100

    feat: add Swagger documentation setup and update dependencies in package.json

--- task_two/server.ts
+++ task_two/server.ts
@@ -3,13 +3,21 @@ import cors from "cors";
 import router from "./routes/routes";
 import express from "express";
 import mongoose from "mongoose";
+// Swagger serve related stuff 
+import swaggerUi from 'swagger-ui-express';
+import YAML from 'yamljs';
+import path from 'path';
+
+
 const PORT = process.env.PORT || 8000
 const app = express()
 
 app.use(cors())
 app.use(express.json())
 app.use("/api/v1", router);
-
+// Serve Swagger stuff on a special route
+const swaggerDocument = YAML.load(path.join(__dirname, '../docs/specs.yml'))
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 // Connect to database
 (async () => {
     try {
