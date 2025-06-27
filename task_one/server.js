c3e18bb7d9db2f4b175910ffd934cccb044804c3
whotterre <iJeddy5.0@gmail.com>
2025-06-27 13:37:36 +0100

    Add OpenAPI documentation for quotes API and integrate Swagger UI in server

--- task_one/server.js
+++ task_one/server.js
@@ -3,13 +3,17 @@
 import cors from "cors";
 import router from "./routes/routes.js";
 import express from "express";
+import swaggerUi from "swagger-ui-express";
+const swaggerDocument = require('../docs/specs.json');
 
 const PORT = process.env.PORT || 8000;
 const app = express();
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use(cors())
 app.use(express.json())
 app.use(router)
+
 
 
 app.listen(PORT, () => {
